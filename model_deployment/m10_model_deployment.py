66a1bb259d46c34a65a4aa86368b0950178e3fb8
Juan Sebastian <34700595+JuanSebas7ian@users.noreply.github.com>
2022-05-01 11:57:33 -0500

    Create m10_model_deployment.py

---
+++ model_deployment/m10_model_deployment.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+import joblib
+import sys
+import os
+
+def predict_proba(Year,Mileage,State,Make,Model):
+
+    clf = joblib.load(os.path.dirname(__file__) + '/phishing_clf.pkl') 
+    d = {'Year': [Year], 'Mileage': [Mileage],'State': [State], 'Make': [Make], 'Model': [Model]}
+    df = pd.DataFrame(data=d)
+    
+  
+    # Create features
+    # Codificación variables categóricas
+    le_State = LabelEncoder()
+    le_Make = LabelEncoder()
+    le_Model = LabelEncoder()
+    df['State_encoded'] = le_State.fit_transform(df.State)
+    df['Make_encoded'] = le_Make.fit_transform(df.Make)
+    df['Model_encoded'] = le_Model.fit_transform(df.Model)
+    df_2=df[['Year',	'Mileage',	'State_encoded','Make_encoded',	'Model_encoded']]
+
+    # Make prediction
+    p1 = rfreg.predict(df)
+
+    return p1
+
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) == 1:
+        print('Please add Year,Mileage,State,Make,Model')
+        
+    else:
+
+        url = sys.argv[1]
+
+        p1 = predict_proba(df_2)
+        
+        print(df)
+        print('Probability of Phishing: ', p1)
