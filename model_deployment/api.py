633578bcaf94fe56eed7af4c56e976621a94c973
Juan Sebastian <34700595+JuanSebas7ian@users.noreply.github.com>
2022-05-01 21:38:27 -0500

    Update api.py

--- model_deployment/api.py
+++ model_deployment/api.py
@@ -21,35 +21,35 @@ parser.add_argument(
     "Year",   
     type=int, 
     required=True, 
-    help='Please add Year, Mileage, State, Make, Model', 
+    help='Please add Year', 
     location='args')
 
 parser.add_argument(
     "Mileage",   
     type=int, 
     required=True,
-    help='Please add Year,Mileage,State,Make,Model', 
+    help='Please add Mileage', 
     location='args')
 
 parser.add_argument(
     "State",   
     type=str, 
     required=True, 
-    help='Please add Year,Mileage,State,Make,Model', 
+    help='Please add State', 
     location='args')
 
 parser.add_argument(
     "Make",   
     type=str, 
     required=True, 
-    help='Please add Year,Mileage,State,Make,Model', 
+    help='Please add Make', 
     location='args')
 
 parser.add_argument(
     "Model",   
     type=str, 
     required=True, 
-    help='Please add Year,Mileage,State,Make,Model', 
+    help='Please add Model', 
     location='args')
 
 
