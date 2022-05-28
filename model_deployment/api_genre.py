6cb6e8566f9ccafd2654c44ad1f5d23218cc9fe1
Juan Sebastian <34700595+JuanSebas7ian@users.noreply.github.com>
2022-05-28 16:14:33 -0500

    Create api_genre.py

---
+++ model_deployment/api_genre.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+from flask import Flask
+from flask_restplus import Api, Resource, fields
+import joblib
+from m11_model_deployment import predict_proba
+
+app = Flask(__name__)
+
+api = Api(
+    app, 
+    version='1.0', 
+    title='Genre Classification',
+    description='Genre Classification API')
+
+ns = api.namespace('predict', 
+     description='Genre Classification')
+   
+parser = api.parser()
+
+parser.add_argument(
+    'Plot',   
+    type=str, 
+    required=True, 
+    help='Enter the Movie Description', 
+    location='args')
+
+
+resource_fields = api.model('Resource', {
+    'text': fields.String,
+})
+
+@ns.route('/')
+class Genre_Proba_Api(Resource):
+
+    @api.doc(parser=parser)
+    @api.marshal_with(resource_fields)
+    def get(self):
+        args = parser.parse_args()
+        
+        return {
+         "result": predict_proba(args['text'])
+        }, 200
+    
+    
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=8888)
